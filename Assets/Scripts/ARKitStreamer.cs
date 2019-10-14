cd574e424c383f46ad8a6ef3c56f4d771cb0f825
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-10-14 15:57:32 +0200

    Toggle stream mode 

--- Assets/Scripts/ARKitStreamer.cs
+++ Assets/Scripts/ARKitStreamer.cs
@@ -10,7 +10,27 @@ public class ARKitStreamer : MonoBehaviour
     [SerializeField] ARHumanBodyManager humanBodyManager = null;
     [SerializeField] Material material = null;
 
+    enum Mode
+    {
+        Stencil,
+        Depth,
+    }
+    [SerializeField] Mode mode;
+
     static readonly int _textureStencil = Shader.PropertyToID("_textureStencil");
+
+    void Start()
+    {
+        switch (mode)
+        {
+            case Mode.Stencil:
+                OnToggleStencil();
+                break;
+            case Mode.Depth:
+                OnToggleDepth();
+                break;
+        }
+    }
 
     void Update()
     {
@@ -20,7 +40,27 @@ public class ARKitStreamer : MonoBehaviour
             return;
         }
 
-        var stencil = humanBodyManager.humanStencilTexture;
-        material.SetTexture(_textureStencil, stencil);
+        Texture2D texture = null;
+        switch (mode)
+        {
+            case Mode.Stencil:
+                texture = humanBodyManager.humanStencilTexture;
+                break;
+            case Mode.Depth:
+                texture = humanBodyManager.humanDepthTexture;
+                break;
+        }
+        material.SetTexture(_textureStencil, texture);
+    }
+
+    public void OnToggleStencil()
+    {
+        humanBodyManager.humanSegmentationStencilMode = HumanSegmentationMode.FullScreenResolution;
+        humanBodyManager.humanSegmentationDepthMode = HumanSegmentationMode.Disabled;
+    }
+    public void OnToggleDepth()
+    {
+        humanBodyManager.humanSegmentationStencilMode = HumanSegmentationMode.Disabled;
+        humanBodyManager.humanSegmentationDepthMode = HumanSegmentationMode.FullScreenResolution;
     }
 }
