e2e0a62463691a9ad3279a219bae0bc868815012
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-10-14 15:34:43 +0200

    Copy from my former repo

--- Assets/Scripts/ARKitStreamer.cs
+++ Assets/Scripts/ARKitStreamer.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class ARKitStreamer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] ARHumanBodyManager humanBodyManager = null;
+    [SerializeField] Material material = null;
 
-    // Update is called once per frame
+    static readonly int _textureStencil = Shader.PropertyToID("_textureStencil");
+
     void Update()
     {
-        
+        var subsystem = humanBodyManager.subsystem;
+        if (subsystem == null)
+        {
+            return;
+        }
+
+        var stencil = humanBodyManager.humanStencilTexture;
+        material.SetTexture(_textureStencil, stencil);
     }
 }
