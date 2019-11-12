3224134f574a36a61b3c5cb866c6ee700857b173
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-11-12 15:07:02 +0100

    Move files

--- Packages/com.koki-ibukuro.arkitstream/Runtime/ARKitSender.cs
+++ Packages/com.koki-ibukuro.arkitstream/Runtime/ARKitSender.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using Klak.Ndi;
 using WebSocketSharp;
