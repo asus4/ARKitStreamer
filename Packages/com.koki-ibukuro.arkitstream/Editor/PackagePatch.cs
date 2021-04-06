488130bf7b0db3fd2eb6b61600f5b7541fd9c73e
Koki Ibukuro <koki.ibukuro@gmail.com>
2021-04-06 15:02:02 +0200

    Apply patch file using C#

--- Packages/com.koki-ibukuro.arkitstream/Editor/PackagePatch.cs
+++ Packages/com.koki-ibukuro.arkitstream/Editor/PackagePatch.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Diagnostics;
 using System.IO;
-using UnityEngine;
+using System.Linq;
 using UnityEditor;
-
 using Debug = UnityEngine.Debug;
 
-namespace ARKitStream
+namespace MockARFoundation
 {
     [InitializeOnLoad]
     public static class PackagePatch
@@ -15,51 +13,53 @@ namespace ARKitStream
         {
             string packagePath = Path.GetFullPath("Packages/com.unity.xr.arfoundation/Runtime/AR/");
             string toolsPath = Path.GetFullPath("Packages/com.koki-ibukuro.arkitstream/Tools/");
-            string scriptPath = Path.GetFullPath("Packages/com.koki-ibukuro.arkitstream/Tools/apply_patch.sh");
-
-            RunCommand(
-                scriptPath,
+            ApplyPatch(
                 Path.Combine(packagePath, "ARCameraBackground.cs"),
                 Path.Combine(toolsPath, "ARCameraBackground.cs.patch")
             );
-
-            RunCommand(
-                scriptPath,
+            ApplyPatch(
                 Path.Combine(packagePath, "ARFace.cs"),
                 Path.Combine(toolsPath, "ARFace.cs.patch")
             );
         }
 
-        private static bool RunCommand(string scriptPath, string filePath, string patchPath)
+        private static bool ApplyPatch(string filePath, string patchPath)
         {
-            Debug.Assert(File.Exists(scriptPath), $"File: {scriptPath} not found");
             Debug.Assert(File.Exists(filePath), $"File: {filePath} not found");
             Debug.Assert(File.Exists(patchPath), $"File: {patchPath} not found");
 
-            var info = new ProcessStartInfo()
+            var (originalText, replacingText) = LoadPatch(patchPath);
+            // Debug.Log($"original:\n {originalText}");
+            // Debug.Log($"replace with:\n {replacingText}");
+
+            var source = File.ReadAllText(filePath);
+            if (!source.Contains(originalText))
             {
-                FileName = "/bin/bash",
-                WorkingDirectory = Environment.CurrentDirectory,
-                Arguments = $"{scriptPath} {filePath} {patchPath}",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-            };
-            var process = Process.Start(info);
-            var stdout = process.StandardOutput.ReadToEnd();
-            var stderr = process.StandardError.ReadToEnd();
-            process.WaitForExit();
-
-            // if (!string.IsNullOrWhiteSpace(stdout))
-            // {
-            //     Debug.Log($"stdout: {stdout}");
-            // }
-            // if (!string.IsNullOrWhiteSpace(stderr))
-            // {
-            //     Debug.LogError($"stderr: {stderr}");
-            // }
-
+                return false;
+            }
+            Debug.Log($"A patch applyed : {filePath}");
+            var newSource = source.Replace(originalText, replacingText);
+            // Debug.Assert(source != newSource);
+            File.WriteAllText(filePath, newSource);
             return true;
+        }
+
+        private static Tuple<string, string> LoadPatch(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath)
+                .SkipWhile(line => !line.StartsWith("@@"))
+                .Skip(1);
+
+            var originals = lines
+                .Where(line => !line.StartsWith("+"))
+                .Select(line => line.Substring(1));
+
+            var replaceWith = lines
+                .Where(line => !line.StartsWith("-"))
+                .Select(line => line.Substring(1));
+
+            const string separator = "\n";
+            return new Tuple<string, string>(string.Join(separator, originals), string.Join(separator, replaceWith));
         }
 
     }
--- Packages/com.koki-ibukuro.arkitstream/Tools/apply_patch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-
-if ! patch -R -s -f --dry-run -u $1 < $2; then
-    patch -u $1 < $2
-    echo patch ${2} - applyed to ${1}
-fi
-
